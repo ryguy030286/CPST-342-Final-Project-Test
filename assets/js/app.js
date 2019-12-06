c8c9331792e0c2a4424cafbf2970dfb1b6714090
ryguy030286 <48996096+ryguy030286@users.noreply.github.com>
2019-12-06 14:55:52 -0600

    Success having all the buttons open the same page and pushing a value to a JS variable.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-console.log(c);
+
+function page_open(button_value){
+    //Replaces index page with template page.
+    window.open("template.html", "_self");
+
+    //Sets paragraph text to value of button clicked
+    //Write another function outside of this one and have it execute.
+
+    //document.getElementById("banner").innerHTML = 'help';
+
+    }
+
+   console.log(x);
