9d00fefcb2350eb8634ba801a621213876db6e3b
Ryan <rsabo@luc.edu>
2019-12-07 15:33:34 -0600

    worked on structure for template page, added a return to index button to it.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,29 @@
 
-function page_open(button_value){
+// Loads template page and passes button value.
+function cookie_page(button_value) {
     //Replaces index page with template page.
     window.open("template.html", "_self");
-
-    //Sets paragraph text to value of button clicked
-    //Write another function outside of this one and have it execute.
-
-    //document.getElementById("banner").innerHTML = 'help';
-
+    localStorage.setItem('page', button_value);
     }
 
-   console.log(x);
+// Returns template page to index page.
+function index_page() {
+    //Replaces index page with template page.
+    window.open("index.html", "_self");
+    }
+
+// Loads value of pressed button.
+    let key = localStorage.getItem('page');
+
+// Set HTML tag to value passed by button press.
+    document.getElementById("title_text").innerHTML = key;
+
+
+//Test to load local JSON
+fetch("./cookie_data.json")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data) {
+       console.log(data);
+    });
