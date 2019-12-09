10fda8428fe7aab3370f2e2d03278513395ff19a
asaucedo1 <asaucedo1@users.noreply.github.com>
2019-12-08 21:43:13 -0600

    Updated bullet points for lists.

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,18 +23,34 @@ fetch("./cookie_data.json")
         return resp.json();
     })
     .then(function(data) {
-        console.log(data[page_name][0].time_table);  // "21 minutes"//
-        console.log(data[page_name][0].instructions);
-        console.log(data[page_name][0].ingredients);
-        console.log(data[page_name][0].needed_equipment);
+        // console.log(data[page_name][0].time_table);  // "21 minutes"//
+        // console.log(data[page_name][0].instructions);
+        // console.log(data[page_name][0].ingredients);
+        // console.log(data[page_name][0].needed_equipment);
         document.getElementById("title_text").innerHTML = data[page_name][0].recipe_name;
         document.getElementById("instructions_text").innerHTML = data[page_name][0].instructions;
-        document.getElementById("ingredients_text").innerHTML = data[page_name][0].ingredients;
-        document.getElementById("equipment_text").innerHTML = data[page_name][0].needed_equipment;
+
+        // Create a list item for each wizard
+        // and append it to the list
+        let ingredient_list = document.getElementById("ingredients_list");
+        data[page_name][0].ingredients.forEach(function (ingredient) {
+            var li = document.createElement('li');
+            li.textContent = ingredient;
+            ingredient_list.appendChild(li);
+        });
+
+        let equipment_list = document.getElementById("equipment_list");
+        data[page_name][0].needed_equipment.forEach(function (equipment) {
+            var li = document.createElement('li');
+            li.textContent = equipment;
+            equipment_list.appendChild(li);
+        });
+
+
         document.getElementById("time_table_text").innerHTML = data[page_name][0].time_table;
 
         let image = document.getElementById("cookie_image");
         image.src = data[page_name][0].finished_product_image_link;
-        image.appendChild(image.src);
+        //image.appendChild(image.src);
 
     });
