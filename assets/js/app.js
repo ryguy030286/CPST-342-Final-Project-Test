9149a41077b2435bd5012913054117aa43d55ce6
asaucedo1 <asaucedo1@users.noreply.github.com>
2019-12-08 22:01:14 -0600

    Updated code for ordered and unordered lists.

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,15 +23,18 @@ fetch("./cookie_data.json")
         return resp.json();
     })
     .then(function(data) {
-        // console.log(data[page_name][0].time_table);  // "21 minutes"//
-        // console.log(data[page_name][0].instructions);
-        // console.log(data[page_name][0].ingredients);
-        // console.log(data[page_name][0].needed_equipment);
         document.getElementById("title_text").innerHTML = data[page_name][0].recipe_name;
-        document.getElementById("instructions_text").innerHTML = data[page_name][0].instructions;
+        document.getElementById("time_table_text").innerHTML = data[page_name][0].time_table;
 
-        // Create a list item for each wizard
-        // and append it to the list
+
+        //Code from: https://gomakethings.com/two-more-ways-to-create-html-from-an-array-of-data-with-vanilla-js/
+        let instruction_list = document.getElementById("instructions_list");
+        data[page_name][0].instructions.forEach(function (instruction) {
+            var li = document.createElement('li');
+            li.textContent = instruction;
+            instruction_list.appendChild(li);
+        });
+
         let ingredient_list = document.getElementById("ingredients_list");
         data[page_name][0].ingredients.forEach(function (ingredient) {
             var li = document.createElement('li');
@@ -45,9 +48,6 @@ fetch("./cookie_data.json")
             li.textContent = equipment;
             equipment_list.appendChild(li);
         });
-
-
-        document.getElementById("time_table_text").innerHTML = data[page_name][0].time_table;
 
         let image = document.getElementById("cookie_image");
         image.src = data[page_name][0].finished_product_image_link;
