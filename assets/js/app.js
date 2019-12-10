8737ef1163c51e90323ec9461a1eab126e8298e7
ryguy030286 <48996096+ryguy030286@users.noreply.github.com>
2019-12-10 14:20:23 -0600

    Created readme file, documented sources, cleaned up/simplified code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,17 @@
 function cookie_page(button_value) {
     //Replaces index page with template page.
     window.open("template.html", "_self");
-    localStorage.setItem('page', button_value);
-    }
+    localStorage.setItem('page', button_value);}
 
 // Returns template page to index page.
 function index_page() {
     //Replaces index page with template page.
-    window.open("index.html", "_self");
-    }
+    window.open("index.html", "_self");}
 
 // Loads value of pressed button.
 let page_name = localStorage.getItem('page');
 
-
 //Loads JSON file using npm json server
-//Instructions were found here https://www.npmjs.com/package/json-server
 fetch("./cookie_data.json")
     .then(function(resp){
         return resp.json();
@@ -25,7 +21,7 @@ fetch("./cookie_data.json")
         document.getElementById("title_text").innerHTML = data[page_name][0].recipe_name;
         document.getElementById("time_table_text").innerHTML = data[page_name][0].time_table;
 
-        //Code from: https://gomakethings.com/two-more-ways-to-create-html-from-an-array-of-data-with-vanilla-js/
+        //Creates and displays instructions.
         let instruction_list = document.getElementById("instructions_list");
         data[page_name][0].instructions.forEach(function (instruction) {
             var li = document.createElement('li');
@@ -33,6 +29,7 @@ fetch("./cookie_data.json")
             instruction_list.appendChild(li);
         });
 
+        //Creates and displays ingredients.
         let ingredient_list = document.getElementById("ingredients_list");
         data[page_name][0].ingredients.forEach(function (ingredient) {
             var li = document.createElement('li');
@@ -40,6 +37,7 @@ fetch("./cookie_data.json")
             ingredient_list.appendChild(li);
         });
 
+        //Creates and displays equipment.
         let equipment_list = document.getElementById("equipment_list");
         data[page_name][0].needed_equipment.forEach(function (equipment) {
             var li = document.createElement('li');
@@ -47,10 +45,10 @@ fetch("./cookie_data.json")
             equipment_list.appendChild(li);
         });
 
-        let image = document.getElementById("cookie_image");
-        image.src = data[page_name][0].finished_product_image_link;
-        //image.appendChild(image.src);
-
+        //Inserts finished product image into template page.
+        document.getElementById("cookie_image").src = data[page_name][0].finished_product_image_link;
     });
 
 textFit(document.querySelector("h3"));
+
+//Around 29 lines of code is used.
